b01839d1025ae79dd6d1a028130cebfc4d02c9f3
M K <mkpro118@gmail.com>
2023-08-24 19:37:13 +0530

    Re-implemented `select_parents`, improved `evolve`

--- genetic_algorithm/population.py
+++ genetic_algorithm/population.py
@@ -1,7 +1,8 @@
-from typing import Optional, Iterable, Callable
+from typing import Callable, Iterable, Optional
 import numpy as np
 
 from genetic_algorithm.genome import Genome
+from genetic_algorithm.selection import selection_methods
 
 
 class Population:
@@ -37,48 +38,59 @@ class Population:
                                   random_state=self._random_state(_)) for _ in range(self.size)]
 
     def select_parents(self, fitness_function: Callable[[Genome], float],
-                       count: Optional[int] = None) -> list[Genome]:
+                       method: str = 'roulette',
+                       count: Optional[int] = None,
+                       random_state: Optional[int] = None,
+                       **kwargs) -> list[Genome]:
+        # Validate fitness_function
+        if not callable(fitness_function):
+            raise TypeError(
+                '`fitness_function` must be a callable that returns a fitness score, '
+                f'found type {type(fitness_function)}.'
+            )
+
         # Calculate the fitness for each genome
         scores: Iterable = map(fitness_function, self.population)
 
         # int is a valid type for param `count`. Mypy expects a SupportsIndex
         scores = np.fromiter(scores, count=self.size)  # type: ignore
 
-        # Perform roulette wheel selection based on fitness scores
-        total_fitness: float = np.sum(scores)
+        try:
+            return selection_methods[method](self.population,
+                                             scores,
+                                             count=count,
+                                             random_state=random_state,
+                                             **kwargs)
+        except KeyError:
+            raise ValueError(
+                f'`{method=}` is not a known selection method'
+            )
 
-        selection_probs = np.divide(scores, total_fitness, out=scores)
+    def evolve(self, fitness_function: Callable[[Genome], float],
+               selection_method: str = 'roulette',
+               selection_count: Optional[int] = None,
+               crossover_method: str = 'one_point',
+               random_state: Optional[int] = None, **kwargs):
 
-        def rand():
-            return self._rng.choice(range(self.size), p=selection_probs)
+        rate = kwargs.get('mutation_rate', self.mutation_rate)
+        amount = kwargs.get('mutation_amount', self.mutation_amount)
 
-        if count is not None:
-            if not isinstance(count, int):
-                raise TypeError(
-                    f'`count` must be an int, found type `{type(count)}`.'
-                )
-            if count < 0:
-                raise ValueError(
-                    f'`count` must be greater than or equal to 0. found `{count=}`.'
-                )
-        else:
-            count = self.size
+        selected_parents = self.select_parents(fitness_function,
+                                               method=selection_method,
+                                               count=selection_count,
+                                               random_state=random_state)
 
-        selected_parents = [self.population[rand()] for _ in range(self.size)]
-
-        return selected_parents
-
-    def evolve(self):
-        selected_parents = self.select_parents()
-
-        new_population = []
+        new_population: list[Genome] = []
 
         while len(new_population) < self.size:
             parent1 = self._rng.choice(selected_parents)
             parent2 = self._rng.choice(selected_parents)
 
-            child = parent1.crossover(parent2)
-            child.mutate(self.mutation_rate, self.mutation_amount)
+            child = parent1.crossover(parent2,
+                                      method=crossover_method,
+                                      **kwargs)
+
+            child.mutate(rate, amount)
 
             new_population.append(child)
 
